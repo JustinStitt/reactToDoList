6b5ca4e13a1a81630bd0a900cd94584c714fa8e9
Justin Stitt <cpt.crixus@gmail.com>
2020-10-14 00:00:26 -0700

    Updates

---
+++ precache-manifest.105f8a6b34c4b2cb6670086dad70ea21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bc9425c095030f761abd7c29ed3c67",
+    "url": "/reactToDoList/index.html"
+  },
+  {
+    "revision": "ee61c2c96794e52147c2",
+    "url": "/reactToDoList/static/css/main.f78e6d27.chunk.css"
+  },
+  {
+    "revision": "0cf3b04c6caf3013f597",
+    "url": "/reactToDoList/static/js/2.cab470fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactToDoList/static/js/2.cab470fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee61c2c96794e52147c2",
+    "url": "/reactToDoList/static/js/main.75d2e06e.chunk.js"
+  },
+  {
+    "revision": "ef7f858d9384b51b48c3",
+    "url": "/reactToDoList/static/js/runtime-main.4a87337b.js"
+  }
+]);
